f45415a7f8ce22edac2d9e48b08bc139fa00ebf2
myersb <Ben@mimac.cmhcorp.local>
2014-10-03 11:01:05 -0400

    Initial Commit

---
+++ VideoPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VideoPlayer
+//
+//  Created by Benjamin Myers on 10/3/14.
+//  Copyright (c) 2014 AppGuys.biz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ VideoPlayer/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  VideoPlayer
+//
+//  Created by Benjamin Myers on 10/3/14.
+//  Copyright (c) 2014 AppGuys.biz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
